930c618b67c0b4ec2c0cc3dd6a7fbbf7615ee8f3
sebstianzamora <69611081+sebstianzamora@users.noreply.github.com>
2025-01-02 22:21:35 -0500

    limpieza-de-datos

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,7 +1,8 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
-
+import pandas as pd
+import os
 
 def pregunta_01():
     """
@@ -13,3 +14,57 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+
+    # Rutas de entrada y salida
+    input_file = "files/input/solicitudes_de_credito.csv"
+    output_dir = "files/output/"
+    output_file = os.path.join(output_dir, "solicitudes_de_credito.csv")
+
+    # Asegurarse de que el directorio de salida exista
+    os.makedirs(output_dir, exist_ok=True)
+
+    try:
+        # Cargar los datos en un DataFrame
+        df = pd.read_csv(input_file, sep=";", index_col=0)
+
+        
+        # separaciones por espacios
+        df.replace({"-": " ", "_": " "}, regex=True, inplace=True)
+
+        # mayusculas y minusculas
+
+        df["sexo"] = df["sexo"].str.lower()
+        df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+        df["barrio"] = df["barrio"].str.lower()
+
+        df["idea_negocio"] = df["idea_negocio"].str.lower().str.strip()
+        df["línea_credito"] = df["línea_credito"].str.lower().str.strip()
+
+
+        df["fecha_de_beneficio"] = pd.to_datetime(
+            df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+        ).combine_first(pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce"))
+
+        df["monto_del_credito"] = (
+            df["monto_del_credito"]
+            .str.replace(r"[,$]", "", regex=True)
+            .str.replace(r"\.00$", "", regex=True)
+            .astype(float)
+        )
+
+        # Eliminar registros duplicados
+        df = df.drop_duplicates()
+
+        # Eliminar registros con datos faltantes
+        df = df.dropna()
+
+        # Guardar el archivo limpio
+        df.to_csv(output_file, index=False, sep=";")
+
+        print(f"Archivo limpio guardado en: {output_file}")
+    except Exception as e:
+        print(f"Error procesando el archivo: {e}")
+
+    print(df.sexo.value_counts().to_list())
+
+pregunta_01()
